6960f5aca68e014f79e3e15b3d6c47b315f0e07d
ali012210 <91623709+ali012210@users.noreply.github.com>
2023-04-09 08:34:03 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/ali-and-marsy-small.jpg") {
-    myImage.setAttribute("src", "images/ali-and-marsy-wave.jpg");
+  if (mySrc === "ali-and-marsy-small.jpg") {
+    myImage.setAttribute("src", "ali-and-marsy-wave.jpg");
   } else {
-    myImage.setAttribute("src", "images/ali-and-marsy-small.jpg");
+    myImage.setAttribute("src", "ali-and-marsy-small.jpg");
   }
 };
